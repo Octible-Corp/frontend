a66e45a15ae58cb5802803d01398f82eba75e8fb
fpvrc <jari@4midableimagery.com>
2021-04-27 16:59:10 -0700

    cleanup

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import store from '../store';
-//import { LOGOUT } from '../actions/types';
 const localhost = 'localhost:5000';
 const serverIP = 'octible.io';
 
@@ -10,22 +8,5 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
-/**
- intercept any error responses from the api
- and check if the token is no longer valid.
- ie. Token has expired
- logout the user if the token has expired
-**/
-/*
-api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response.data.msg === 'Token is not valid') {
-      store.dispatch({ type: LOGOUT });
-    }
-    return Promise.reject(err);
-  }
-);
-*/
 
 export default api;
