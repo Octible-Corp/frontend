136926e089c7fec33b76468954d945a4097866cd
fpvrc <jari@4midableimagery.com>
2021-04-30 17:42:21 -0700

    update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const localhost = 'localhost:5000';
-const serverIP = '54.176.187.141';
+const serverIP = '54.241.77.78';
 
 const api = axios.create({
   baseURL: `http://${serverIP}/`,
